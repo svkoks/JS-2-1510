dfb2e0539c8fa810c39328cb235812c1931e72e3
pavelWork <exciland@yandex.ru>
2020-10-31 18:57:06 +0300

    made on class

--- 3-STUDENTS/Pavel_Kulakouski/Project/src/components/catalog.js
+++ 3-STUDENTS/Pavel_Kulakouski/Project/src/components/catalog.js
@@ -1,17 +1,14 @@
-// const NAMES = ['Mango People T-Shirt', 'Banana People T-Shirt', 'Strawberry People T-Shirt', 'Orange People T-Shirt', 'Pumpkin People T-Shirt', 'Pineapple People T-Shirt', 'Cucumber People T-Shirt', 'Tomato People T-Shirt'];
-// const PRICES = [52, 53, 55, 67, 69, 94, 23, 45];
-
-//
-export default {
-    items: [],
-    basket: null,
-    container: null,
-    imgFTPurl: 'https://raw.githubusercontent.com/kellolo/static/master/img/JS1_shop',
-    url: 'https://raw.githubusercontent.com/kellolo/static/master/JSON/catalog.json',
-    init(basket) {
-        this.container = document.querySelector('#catalog');
+export default class Catalog {
+    constructor(basket, container = '#catalog', url = '/catalog.json') {
+        this.items = [];
         this.basket = basket;
-        // this.items = getItems();
+        this.wrapper = null;
+        this.container = document.querySelector(container);
+        this.url = 'https://raw.githubusercontent.com/kellolo/static/master/JSON' + url;
+        this.imgFTPurl = 'https://raw.githubusercontent.com/kellolo/static/master/img/JS1_shop';
+        this._init();
+    }
+    _init() {
         this._get(this.url)
         .then(items => {
             this.items = items; 
@@ -22,10 +19,10 @@ export default {
         })
 
         // this._handleEvents();
-    },
+    }
     _get(url) {
         return fetch(url).then(d => d.json()) //на выходе из этого метода вы получите полноценный объект(массив) с данными
-    },
+    }
     _handleEvents() {
         this.container.addEventListener('click', e => {
             if (e.target.name == 'add') {
@@ -38,7 +35,7 @@ export default {
                 this.basket.add(item)
             }
         })
-    },
+    }
     _render() {
         let htmlStr = '';
         this.items.forEach((item, index) => {
