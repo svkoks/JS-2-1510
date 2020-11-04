ee04666781846b84447c23966bf67af42f684048
Gleb <karbonelg@mail.ru>
2020-11-04 06:42:04 +0300

    Импорт, инициализация и экспорт классов, динамически добавлющих пункты меню на страницу.

--- 3-STUDENTS/Карбонель Глеб/Project/src/components/index.js
+++ 3-STUDENTS/Карбонель Глеб/Project/src/components/index.js
@@ -1,7 +1,11 @@
-import Basket from './basket.js';
-import Catalog from './catalog.js';
+import { Basket } from './basket.js';
+import { Catalog } from './catalog.js';
+import { mainMenuItems } from './mainMenuItems.js';
+import { footerMenuItems } from './footerMenuItems.js';
 
 export default () => {
     let basket = new Basket();
     let catalog = new Catalog(basket);
+    let mainMenu = new mainMenuItems();
+    let footerMenu = new footerMenuItems();
 }
