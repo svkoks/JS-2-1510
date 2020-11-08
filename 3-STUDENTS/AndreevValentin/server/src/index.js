9b41cd63ad622016f9b013ced18860ee1a6b7d02
voidf.0xb3 <the.invisible.phantom@gmail.com>
2020-11-09 01:25:13 +0300

    Serve catalog.json from our server

--- 3-STUDENTS/AndreevValentin/server/src/index.js
+++ 3-STUDENTS/AndreevValentin/server/src/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const fs = require("fs");
 
 const server = express();
+
+server.get("/catalog.json", async (req, res) => {
+	fs.readFile("./src/db/catalog.json", (err, data) => {
+		if(err) {
+			throw err;
+		}
+		res.header("Content-Type", "application/json").send(data);
+	});
+});
 
 server.listen(3300, () => {
 	console.log("Listening");
