960284cd78bdeffe7ccdf99f66902949a06999a4
IlyaVoronOFF <iluha.voronov@yandex.ru>
2020-11-08 03:53:36 +0300

    VueCLI

---
+++ 3-STUDENTS/Воронов Илья/project-vue-cli/src/components/Catalog.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="catalog">
+    <div class="product-items" id="catalog-index">
+      <Item v-for="item of items" :key="item.productId" :item="item" />
+    </div>
+    <a href="#" class="product-btn"
+      >Browse All Product <i class="fas fa-long-arrow-alt-right"></i
+    ></a>
+  </div>
+</template>
+
+<script>
+import { get } from "@/core/requests";
+import Item from "./CatalogItem";
+
+export default {
+  data() {
+    return {
+      items: [],
+      url:
+        "https://raw.githubusercontent.com/IlyaVoronOFF/JSON/master/Brand/catalog.json",
+    };
+  },
+  components: { Item },
+
+  async mounted() {
+    try {
+      this.items = await get(this.url);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      //console.log(await this._get(this.basket.url));
+    }
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 3-STUDENTS/Воронов Илья/project-vue-cli/src/core/requests/index.js
@@ -0,0 +1,15 @@
+export function get(url) {
+    return fetch(url).then((d) => d.json());
+}
+
+export function post() {
+    //   return fetch(url).then((d) => d.json());
+}
+
+export function put() {
+    //   return fetch(url).then((d) => d.json());
+}
+
+export function deleteReq() {
+    //   return fetch(url).then((d) => d.json());
+}
