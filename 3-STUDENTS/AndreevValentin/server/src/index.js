53198873c85c0068ffb946c70d90d168feac8588
voidf.0xb3 <the.invisible.phantom@gmail.com>
2020-11-09 20:11:43 +0300

    Implement cart PUT & DELETE APIs

--- 3-STUDENTS/AndreevValentin/server/src/index.js
+++ 3-STUDENTS/AndreevValentin/server/src/index.js
@@ -8,24 +8,39 @@ server.use(express.json());
 const fsReadFile = util.promisify(fs.readFile);
 const fsWriteFile = util.promisify(fs.writeFile);
 
+const catalogFile = "./src/db/catalog.json";
+const cartFile = "./src/db/cart.json";
+
 server.get("/catalog", async (req, res) => {
-	const data = await fsReadFile("./src/db/catalog.json");
+	const data = await fsReadFile(catalogFile);
 	res.header("Content-Type", "application/json").send(data);
 });
 
 server.get("/cart", async (req, res) => {
-	const data = await fsReadFile("./src/db/cart.json");
+	const data = await fsReadFile(cartFile);
 	res.header("Content-Type", "application/json").send(data);
 });
 
-server.put("/cart/:id", (req, res) => {
-	console.log("put", req.params.id, req.body);
-	res.send("");
+server.put("/cart/:id", async (req, res) => {
+	let data = JSON.parse(await fsReadFile(cartFile));
+	const index = data.findIndex(item => item.id == req.params.id);
+	if(index == -1) {
+		data.push(req.body);
+	} else {
+		data[index] = req.body;
+	}
+	console.log(cartFile, JSON.stringify(data));
+	await fsWriteFile(cartFile, JSON.stringify(data));
+	res.send();
 });
 
-server.delete("/cart/:id", (req, res) => {
-	console.log("delete", req.params.id);
-	res.send("");
+server.delete("/cart/:id", async (req, res) => {
+	let data = JSON.parse(await fsReadFile(cartFile));
+	const index = data.findIndex(item => item.id == req.params.id);
+	data.splice(index, 1);
+	console.log(cartFile, JSON.stringify(data));
+	await fsWriteFile(cartFile, JSON.stringify(data));
+	res.send();
 })
 
 server.listen(3300, () => {
