87f06f328a6fe913350bb1955daa49da4fa2e8fb
Yuri Tarelkin <tarelkinyuri@gmail.com>
2020-11-11 22:57:19 +0300

    Merge pull request #5 from kellolo/master
    merge

---
+++ 3-STUDENTS/YuriTarelkin/project-vue-cli/src/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+
+import './layout/style/css/style.css'
+import App from './components'
+
+import App from 'App.vue';
+
+new Vue({
+    render: h => h(App)
+}).$mount('#app')
