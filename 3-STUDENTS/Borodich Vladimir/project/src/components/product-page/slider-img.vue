41d857451d9a3d550bbaec14b697390588d79bd1
wowankz <wowikwow@mail.ru>
2020-11-12 02:52:45 +0500

    псевдо анимация загрузки изображения

--- 3-STUDENTS/Borodich Vladimir/project/src/components/product-page/slider-img.vue
+++ 3-STUDENTS/Borodich Vladimir/project/src/components/product-page/slider-img.vue
@@ -1,17 +1,26 @@
 <template>
     <div class="product__slide-img">
         <a 
-         v-bind:class="{ disabled: imgNum === 0 }"
-        @click.prevent="changeImg(-1)" href="#" 
-        class="product__slide-link product__slide-link_left">
-            <i class="fas fa-angle-left"></i>
+             v-bind:class="{ disabled: imgNum === 0 }"
+            @click.prevent="changeImg(-1)" href="#" 
+            class="product__slide-link product__slide-link_left">
+                <i class="fas fa-angle-left"></i>
         </a>
-        <img :src="`https://raw.githubusercontent.com/wowankz/static/master/shop/img/${img}`" alt="product" class="product__img" />
+
+        <div v-show="loadImg" class="load-img"><i class="fas fa-spinner fa-spin"></i></div>
+
+        <img
+            v-show="!loadImg"
+            :onload="loaded()" 
+            :src="`https://raw.githubusercontent.com/wowankz/static/master/shop/img/${img}`" 
+            alt="product" 
+            class="product__img" />
+
         <a 
-        v-bind:class="{ disabled: imgNum === (product.sliderImg.length -1) }"
-        @click.prevent="changeImg(1)" href="#" 
-        class="product__slide-link product__slide-link_right">
-            <i class="fas fa-angle-right"></i>
+            v-bind:class="{ disabled: imgNum === (product.sliderImg.length -1) }"
+            @click.prevent="changeImg(1)" href="#" 
+            class="product__slide-link product__slide-link_right">
+                <i class="fas fa-angle-right"></i>
         </a>
     </div>
 </template>
@@ -26,14 +35,23 @@ export default {
         return {
             img: 'slider-img.jpg',
             imgNum: 0,
+            loadImg:true
         };
     },
 
     methods: {
         changeImg(val) {
+            console.log(this.loadImg);
+            this.loadImg = true;
             this.imgNum += val;
+            console.log(this.loadImg);
             this.img = this.product.sliderImg[this.imgNum];
         },
+
+        loaded(){
+            console.log('loaded');
+           setTimeout(()=>{this.loadImg = false},300) ;
+        }
     },
 
     mounted() {
@@ -42,4 +60,13 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.load-img{
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    font-size: 5rem;
+    color: #9E9E9E;
+    font-size: 5rem;
+}
+</style>
